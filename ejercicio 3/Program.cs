64e12b13577dfd896dc1ece907b5a745ef98f8b4
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-03-04 21:21:01 -0600

    ejercicio 2

---
+++ ejercicio 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> notas = new Dictionary<string, double>();
+
+            notas.Add("Marlon", 85);
+            notas.Add("Julia", 90);
+            notas.Add("Jose", 75);
+
+            Console.WriteLine("Nota de Julia: " + notas["Julia"]);
+        }
+    }
+}

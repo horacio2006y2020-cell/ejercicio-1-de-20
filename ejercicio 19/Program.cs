e30ee3ec519e57eb1d3e4a14a9693357b4b3b6ed
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-03-04 23:40:21 -0600

    ejercicio19

---
+++ ejercicio 19/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+              Dictionary<string, double> notas = new Dictionary<string, double>();
+
+        notas.Add("Juan", 80);
+
+        Console.Write("Nombre: ");
+        string nombre = Console.ReadLine();
+
+        if (notas.ContainsKey(nombre))
+        {
+            Console.Write("Nueva nota: ");
+            double nota = double.Parse(Console.ReadLine());
+            notas[nombre] = nota;
+        }
+
+        foreach (var item in notas)
+            Console.WriteLine(item.Key + " " + item.Value);
+        }
+    }
+}

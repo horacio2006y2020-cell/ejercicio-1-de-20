fb9aad19f7b8fbc214459f69a7b6b21e0a408ff3
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-03-04 21:14:08 -0600

    ejercicio 2

---
+++ ejercicio 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> productos = new Dictionary<int, string>();
+
+            productos.Add(101, "Laptop");
+            productos.Add(102, "Mouse");
+            productos.Add(103, "Teclado");
+
+            foreach (var item in productos)
+                Console.WriteLine("Codigo: " + item.Key + " Producto: " + item.Value);
+        }
+    }
+}

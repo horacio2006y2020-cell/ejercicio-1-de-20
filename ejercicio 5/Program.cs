cefae4c7e59868e31a023dbeaaca5da18ded3795
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-03-04 21:46:28 -0600

    ejerccicio 5

---
+++ ejercicio 5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> usuarios = new Dictionary<string, string>();
+
+            usuarios.Add("admin", "1234");
+            usuarios.Add("jose", "abcd");
+
+            Console.WriteLine("Contraseña de jose: " + usuarios["jose"]);
+        }
+    }
+}

05776def794c82c9a774a8105ff0cd2f499d15fc
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-03-04 23:05:58 -0600

    ejercicio 13

---
+++ ejercicio 13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> notas = new Dictionary<string, double>()
+        {
+            {"Juan",80},
+            {"Maria",90},
+            {"Carlos",70}
+        };
+
+            double suma = 0;
+
+            foreach (var item in notas)
+                suma += item.Value;
+
+            Console.WriteLine("Promedio: " + (suma / notas.Count));
+        }
+    }
+}

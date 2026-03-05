63aa25cf0f3f074dc01257b32c716de80d89593c
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-03-04 23:48:16 -0600

    parte 1

---
+++ 20/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, (string Nombre, int Edad, string Carrera)> estudiantes =
+           new Dictionary<int, (string, int, string)>();
+
+            estudiantes.Add(1001, ("Ana", 20, "Ingenieria"));
+            estudiantes.Add(1002, ("Carlos", 22, "Derecho"));
+            estudiantes.Add(1003, ("Maria", 19, "Medicina"));
+        }
+    }
+}

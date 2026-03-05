3d36583956bd6468e420bca2cf4f3bef0d638235
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-03-04 23:19:33 -0600

    ejercicio 15

---
+++ ejercicio 15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> empleados = new Dictionary<int, string>()
+        {
+            {1,"Luis"},
+            {2,"Pedro"}
+        };
+
+            foreach (var item in empleados)
+                Console.WriteLine("ID: " + item.Key + " Nombre: " + item.Value);
+        }
+    }
+}

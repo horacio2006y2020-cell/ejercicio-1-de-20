d2c41df252299dff1a7a5740b7b4eb0c4701a8d4
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-03-04 21:24:07 -0600

    ejercicio 3

---
+++ ejercicio 4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> ciudades = new Dictionary<int, string>();
+
+            ciudades.Add(1, "Guatemala");
+            ciudades.Add(2, "Antigua");
+            ciudades.Add(3, "Peten");
+        }
+    }
+}

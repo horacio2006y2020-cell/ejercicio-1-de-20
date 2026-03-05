f483132a68f82a054315b78adb8e248af525eac1
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-03-04 21:58:31 -0600

    ejercicio 6

---
+++ ejercicio 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> estudiantes = new Dictionary<int, string>();
+
+            estudiantes.Add(1, "Juan");
+            estudiantes.Add(2, "Maria");
+            estudiantes.Add(3, "Carlos");
+
+            estudiantes.Remove(2);
+
+            foreach (var item in estudiantes)
+                Console.WriteLine(item.Key + " " + item.Value);
+        }
+    }
+}

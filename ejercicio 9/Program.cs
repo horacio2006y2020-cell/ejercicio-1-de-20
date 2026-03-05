a963b4660f9a15cbd46e584f1630990ad397afc6
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-03-04 22:34:41 -0600

    ejercicio 9

---
+++ ejercicio 9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> inventario = new Dictionary<int, string>();
+
+            inventario.Add(1, "Laptop");
+            inventario.Add(2, "Mouse");
+
+            Console.Write("Codigo a eliminar: ");
+            int codigo = int.Parse(Console.ReadLine());
+
+            if (inventario.ContainsKey(codigo))
+                inventario.Remove(codigo);
+        }
+    }
+}

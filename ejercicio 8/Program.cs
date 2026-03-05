6c945b585dd9d0081acff377a13a45907ca174b5
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-03-04 22:27:11 -0600

    ejercicio 8

---
+++ ejercicio 8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> empleados = new Dictionary<int, string>();
+
+            empleados.Add(1, "Luis");
+
+            empleados[1] = "Pedro";
+
+            Console.WriteLine(empleados[1]);
+        }
+    }
+}

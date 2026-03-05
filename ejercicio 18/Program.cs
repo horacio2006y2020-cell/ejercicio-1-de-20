8213042f2ecb25b42a8274a467c4158053844d70
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-03-04 23:35:12 -0600

    18

---
+++ ejercicio 18/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> usuarios = new Dictionary<string, string>();
+
+            Console.Write("Usuario: ");
+            string user = Console.ReadLine();
+
+            if (usuarios.ContainsKey(user))
+                Console.WriteLine("Ya existe.");
+            else
+            {
+                Console.Write("Contraseña: ");
+                string pass = Console.ReadLine();
+                usuarios.Add(user, pass);
+            }
+    }
+}

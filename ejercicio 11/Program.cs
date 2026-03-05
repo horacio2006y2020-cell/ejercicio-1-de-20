3c14fd573e863b712d178b4c0f57230a1960ea41
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-03-04 22:56:13 -0600

    ejercicio 12

---
+++ ejercicio 11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> productos = new Dictionary<int, string>()
+        {
+            {101,"Laptop"},
+            {102,"Mouse"},
+            {103,"Teclado"}
+        };
+
+            foreach (var item in productos)
+                Console.WriteLine(item.Key + " - " + item.Value);
+        }
+    }
+}

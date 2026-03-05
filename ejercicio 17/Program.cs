42555ac244cf4b06d13ec6b093fd4c7b49222806
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-03-04 23:22:11 -0600

    parte 1

---
+++ ejercicio 17/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+              Dictionary<int, string> inventario = new Dictionary<int, string>();
+        int opcion;
+
+        do
+        {
+            Console.WriteLine("\n1 Agregar");
+            Console.WriteLine("2 Eliminar");
+            Console.WriteLine("3 Mostrar");
+            Console.WriteLine("0 Salir");
+            opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+        }
+    }
+}

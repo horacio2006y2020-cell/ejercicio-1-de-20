481d6976715c317b1d7060f4a18066d98258c794
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-03-04 23:17:26 -0600

    ejercicio 14

---
+++ ejercicio 14/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> ciudades = new Dictionary<int, string>()
+        {
+            {1,"Retalhuleu"},
+            {2,"Quetzaltenango"},
+            {3,"Belice"}
+        };
+
+            foreach (var item in ciudades)
+                Console.Write(item.Key + " ");
+
+            Console.WriteLine();
+
+            foreach (var item in ciudades)
+                Console.Write(item.Value + " ");
+        }
+    }
+}
